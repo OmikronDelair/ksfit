d6746bd8ed2bd7b98121d85b4074aca5dcaeea7d
Leonardo Gonzalez <omikron.dementia@gmail.com>
2014-08-11 01:14:21 -0500

    Finish basic upload functionality

--- app/controllers/ksfs_controller.rb
+++ app/controllers/ksfs_controller.rb
@@ -1,4 +1,11 @@
 class KsfsController < ApplicationController
   def index
   end
+
+  def upload
+    uploaded_io = params[:ucs_file]
+    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Ksfit::Application.routes.draw do
    root to: 'ksfs#index'
+   post 'ksfs/upload'
 end
