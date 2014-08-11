4d771f3385ac288c8358c2db432b0e45dad8afcf
Leonardo Gonzalez <omikron.dementia@gmail.com>
2014-08-11 02:13:44 -0500

    Prevent filename duplication

--- app/controllers/ksfs_controller.rb
+++ app/controllers/ksfs_controller.rb
@@ -4,8 +4,22 @@ class KsfsController < ApplicationController
 
   def upload
     uploaded_io = params[:ucs_file]
-    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+    numberized_filename = numberize(uploaded_io.original_filename)
+    file_location = Rails.root.join('public', 'uploads', numberized_filename)
+
+    File.open(file_location, 'wb') do |file|
       file.write(uploaded_io.read)
     end
+
+    respond_to do |format|
+      format.html {redirect_to action: :index}
+      format.json {render json: '[{"file_location" : "' + numberized_filename + '"}]'}
+    end
+  end
+
+  private
+
+  def numberize slug
+    slug + '-' + Time.now.seconds_since_midnight.round.to_s
   end
 end
