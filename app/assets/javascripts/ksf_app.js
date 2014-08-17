67d2dc54ed330fe922e6afca1748ef69e26d6b74
Leonardo Gonzalez <omikron.dementia@gmail.com>
2014-08-17 03:03:33 -0500

    Improve dropdown behavior on view

--- app/assets/javascripts/ksf_app.js
+++ app/assets/javascripts/ksf_app.js
@@ -8,7 +8,15 @@ $(function () {
     done: function (e, data) {
       $.each(data.result.files, function (index, file) {
         $('<p/>').text(file.message[0]).attr('data-ucs-info',file.name).appendTo('#ucs-info');
-        $('select').val(file.message[1]);
+
+        if (file.message[1] == null){
+          $('select').css('display','block');
+        }
+        else{
+          $('select').css('display','none')
+                     .val(file.message[1]);
+        };
+
         $('#file_name').val(file.name);
         $('#song_title').val($(':selected').text());
       });
